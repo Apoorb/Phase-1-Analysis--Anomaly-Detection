7d8e49e7ca9477ad566d2b78e3d5d5f56c233e97
abibeka <apoorb2510@gmail.com>
2018-11-30 21:25:17 -0600

    Plot Out of Control Points

--- T2 Chart.py
+++ T2 Chart.py
@@ -15,9 +15,7 @@ import os
 import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib.pylab as pylab
-
 from scipy.stats import chi2
-
 
 # Corrlation Switch
 Is_Cov =True
@@ -29,7 +27,7 @@ else:
     dat=pd.read_csv("PrinCompDat_Correl.csv")#Correlation
 
 
-def Phase1T2(df,iter1,p=3,alpha=0.05,ulim=40):
+def Phase1T2(df,iter1,OC_pnt,p=3,alpha=0.05,ulim=50):
     Ind=df.obs
     X=df.drop("obs",axis=1).copy()
     S=  np.cov(X.T) #Alternate way to get covariance
@@ -46,16 +44,27 @@ def Phase1T2(df,iter1,p=3,alpha=0.05,ulim=40):
     dof=p
     UCL=chi2.ppf(1-alpha,dof)
     fig,ax = plt.subplots()
-    ax.plot(Ind,T2,marker="o")
+    ax.plot(Ind,T2,marker="o",label="Current Iteration")
     ax.axhline(y=UCL, color='r', linestyle='-')
+    ax.plot(OC_pnt.obs,OC_pnt.T2,'*r',label="Out-Control Points from Previous Iteration")
+    handles, labels = ax.get_legend_handles_labels()
+    # reverse the order
+    ax.legend(handles[::-1], labels[::-1],loc='upper right')
     ax.set(xlabel="Observation",ylabel=r"$T^2$",title=r"$T^2$ Chart with $\alpha$=0.05 (Iteration %s)"%iter1)
     ax.set_ylim(0,ulim)
+#red_dot, = plt.plot(z, "ro", markersize=15)
+## Put a white cross over some of the data.
+#white_cross, = plt.plot(z[:5], "w+", markeredgewidth=3, markersize=15)
+#
+#plt.legend([red_dot, (red_dot, white_cross)], ["Attr A", "Attr A+B"])
+#    
     plt.close()
     # Get In Control Points
     df1=df.copy()
     df1.loc[:,"UCL"]=UCL
     df1.loc[:,"T2"]=T2.reshape(df1.shape[0])
     df1.loc[:,"delRow"]=np.where(df1['T2']>df1['UCL'],True, False)
+    OCpnt_Prev=df1.loc[df1.delRow,["obs","T2"]].copy()
     #Delete the following obs
     df1.loc[df1.delRow,'obs']
     #Deleting the obs
@@ -63,6 +72,7 @@ def Phase1T2(df,iter1,p=3,alpha=0.05,ulim=40):
     res=dict()
     res['Fig']=fig
     res['Ndf']=df1
+    res['PrevOc']=OCpnt_Prev
     return res
 
 
@@ -79,8 +89,10 @@ def Phase1T2(df,iter1,p=3,alpha=0.05,ulim=40):
 
 
 dat.loc[:,"obs"]=dat.index+1
+dat_cpy=dat.copy()
 Di_Res=dict()
 Di_ResF=dict()
+OC_cont=pd.DataFrame({"obs":[],"T2":[]})
 i=1
 params = {'legend.fontsize': 'xx-large',
           'figure.figsize': (15,10),
@@ -90,9 +102,10 @@ params = {'legend.fontsize': 'xx-large',
          'ytick.labelsize':'xx-large'}
 pylab.rcParams.update(params)
 while True:
-    Res1=Phase1T2(df=dat,iter1=i)
+    Res1=Phase1T2(df=dat,iter1=i,OC_pnt=OC_cont)
     Di_Res[i]=Res1["Ndf"]
     Di_ResF[i]=Res1["Fig"]
+    OC_cont=pd.concat([OC_cont,Res1["PrevOc"]])
     if(dat.shape[0]==Res1['Ndf'].shape[0]):
         break
     dat=Res1['Ndf'].loc[:,["obs","PC1","PC2","PC3"]].copy()
@@ -121,4 +134,9 @@ Di_ResF[6]
 Di_ResF[7]
 Di_ResF[8]
 Di_ResF[9]
+
+#******************************************************************************
+IC_dat=np.array(Di_Res[9].obs)
+all_dat=np.array(dat_cpy.obs.tolist())
+OC_obs=np.setdiff1d(all_dat,IC_dat)
 
